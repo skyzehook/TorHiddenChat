cab2e2652b759d90b253c198d5fa770c354c1bbb
skyze <53905274+skyzehook@users.noreply.github.com>
2025-06-28 15:41:47 +0300

    database

---
+++ chatdatabase.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE chatdatabase CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+USE chatdatabase;
+
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE messages (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    message TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
